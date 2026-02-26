2b56bbf516e8563e4606c19a9ee9d2bd688af695
aleksandrvagner <avagner@bmw-tech.de>
2026-02-26 17:51:57 +0300

    fix(monitor): replace syscall constants with golang.org/x/sys/windows constants
    - Updated `execCommandContext` to use `golang.org/x/sys/windows` for `CREATE_NO_WINDOW` and `CREATE_NEW_PROCESS_GROUP`.
    - Ensures compatibility and alignment with the recommended Windows syscall package.

--- internal/monitor/exec_windows.go
+++ internal/monitor/exec_windows.go
@@ -4,6 +4,7 @@ package monitor
 
 import (
 	"context"
+	"golang.org/x/sys/windows"
 	"os/exec"
 	"syscall"
 )
@@ -11,7 +12,7 @@ import (
 func execCommandContext(ctx context.Context, name string, args ...string) *exec.Cmd {
 	cmd := exec.CommandContext(ctx, name, args...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true,
-		CreationFlags: syscall.CREATE_NO_WINDOW | syscall.CREATE_NEW_PROCESS_GROUP}
+		CreationFlags: windows.CREATE_NO_WINDOW | windows.CREATE_NEW_PROCESS_GROUP}
 
 	return cmd
 }

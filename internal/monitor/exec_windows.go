4f5f70ccc8ec436beb6743ed56f2933aa3c0ad46
aleksandrvagner <avagner@bmw-tech.de>
2026-02-26 17:48:36 +0300

    feat(monitor): add process group creation flag for Windows commands
    - Updated `execCommandContext` to include `CREATE_NEW_PROCESS_GROUP` and `CREATE_NO_WINDOW` flags.
    - Ensures better process isolation and hidden window handling in Windows-specific command execution.

--- internal/monitor/exec_windows.go
+++ internal/monitor/exec_windows.go
@@ -10,6 +10,8 @@ import (
 
 func execCommandContext(ctx context.Context, name string, args ...string) *exec.Cmd {
 	cmd := exec.CommandContext(ctx, name, args...)
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true,
+		CreationFlags: syscall.CREATE_NO_WINDOW | syscall.CREATE_NEW_PROCESS_GROUP}
+
 	return cmd
 }
